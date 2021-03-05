db78a47fad997cb87497f77e6e3880f84476cd3b
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-03-05 14:40:44 -0600

    Database setup

--- database.sql
+++ database.sql
@@ -6,5 +6,69 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (100) NOT NULL,
+    "first_name" VARCHAR (100) NOT NULL,
+    "last_name" VARCHAR (100) NOT NULL,
+    "auth_level" INT DEFAULT 0 NOT NULL,
+    "user_image" VARCHAR (1000)
+);
+
+CREATE TABLE "categories" (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+
+CREATE TABLE "items" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "cat_id" INT REFERENCES "categories",
+    "size" INT,
+    "price" INT,
+    "flex" VARCHAR (50),
+    "style" VARCHAR (50),
+    "brand" VARCHAR (50),
+    "shape" VARCHAR (50),
+    "gender" VARCHAR (50),
+    "profile" VARCHAR (50),
+    "condition" VARCHAR (50),
+    "lacing_system" VARCHAR (50),
+    "description" VARCHAR (1000)
+);
+
+CREATE TABLE "images" (
+    "id" SERIAL PRIMARY KEY,
+    "item_id" INT REFERENCES "items",
+    "url" TEXT
+);
+
+
+CREATE TABLE "swaps" (
+    "id" SERIAL PRIMARY KEY,
+    "is_private" BOOLEAN,
+    "start_date" DATE,
+    "sell_date" DATE,
+    "stop_date" DATE,
+    "purchased" BOOLEAN,
+    "access_code" TEXT
+);
+
+
+CREATE TABLE "swap_users" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "swap_id" INT REFERENCES "swaps"
+);
+
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "item_id" INT REFERENCES "items"
+);
+
+CREATE TABLE "swap_item_join" (
+    "id" SERIAL PRIMARY KEY,
+    "item_id" INT REFERENCES "items",
+    "swap_id" INT REFERENCES "swaps"
 );
