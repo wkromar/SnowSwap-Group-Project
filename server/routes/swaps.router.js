49b87b2c63cb5c78edf251f7281f052e39b67efb
Woody Kromar <wkromar12@gmail.com>
2021-03-07 15:32:00 -0600

    post complete

--- server/routes/swaps.router.js
+++ server/routes/swaps.router.js
@@ -23,13 +23,29 @@ router.get("/", rejectUnauthenticated, (req, res) => {
     });
 });
 
-router.post("/", rejectUnauthenticated, (req, res) => {});
-const queryText = `SELECT * FROM "swaps";`;
-pool
-  .query(queryText)
-  .then((result) => {})
-  .catch((error) => {
-    console.log("error in swaps POST", error);
-    res.sendStatus(500);
-  });
+//insert into SWAPS database
+router.post("/", rejectUnauthenticated, (req, res) => {
+  const swapToStore = req.body;
+  console.log("sending swap", swapToStore);
+  const queryText = `INSERT INTO "swaps" ("is_private", "start_date", "sell_date", "stop_date", "swap_open", "access_code")
+        VALUES($1, $2, $3, $4, $5, $6)`;
+  pool
+    .query(queryText, [
+      swap.is_private,
+      swap.start_date,
+      swap.sell_date,
+      swap.stop_date,
+      swap.open,
+      swap.access_code,
+    ])
+    .then((response) => {
+      console.log(response);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("error in swaps POST", error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
