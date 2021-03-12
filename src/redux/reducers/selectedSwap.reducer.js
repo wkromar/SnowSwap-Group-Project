a87db3c4c8d945f1be51797495cfd4c6b5f83a9c
Justin Appelgren <justinappelgren@gmail.com>
2021-03-11 21:06:19 -0600

    Fixed issue with swapitems not mapping

--- src/redux/reducers/selectedSwap.reducer.js
+++ src/redux/reducers/selectedSwap.reducer.js
@@ -1,9 +1,7 @@
 const selectedSwapReducer = (state = {}, action) => {
     switch (action.type) {
-
         case 'SET_SELECTED_SWAP':
             return action.payload;
-
         default:
             return state;
     }
