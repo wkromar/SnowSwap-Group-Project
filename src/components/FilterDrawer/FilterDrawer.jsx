4b24e536e787866a710a1a24a0aeb75e3a1cf61f
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-03-15 09:57:52 -0500

    added variables.

--- src/components/FilterDrawer/FilterDrawer.jsx
+++ src/components/FilterDrawer/FilterDrawer.jsx
@@ -26,9 +26,44 @@ const useStyles = makeStyles({
 
 function FilterDrawer() {
 
+    const flex = ["Stiff", "Semi-stiff", "Mid", "Semi-flex", "Flex"];
+    const snowboardStyle = [
+        "Freestyle",
+        "Freeride",
+        "All-Mountain",
+        "Powder",
+        "Race",
+        "Swallowtail",
+    ];
+    const skiStyle = [
+        "Alpine",
+        "Freeride",
+        "Telemark",
+        "Cross-country",
+        "Freestyle",
+        "Racing",
+        "Powderhound Planks",
+    ];
+    const shape = ["Directional", "Directional Twin", "Twin", "Volume Shifted"];
+    const profile = ["Camber", "Camber rocker combo", "Rocker", "Reverse Camber"];
+    const condition = [
+        "Boneyard",
+        "Heavily used",
+        "Moderately used",
+        "Lightly used",
+        "Like new",
+        "New",
+    ];
+    const lacing_system = ["Traditional", "Quick-pull", "BOA"];
+    const size = ["XS", "S", "M", "L", "XL", "XXL"]
+
     const [isOpen, setIsOpen] = useState(false);
+
     const [filter, setFilter] = useState('');
-    const [category, setCategory] = useState('')
+    const [category, setCategory] = useState('');
+    const [snowboardStyleSearch, setSnowboardStyleSearch] = useState('');
+    const [skiStyleSearch, setSnowboardStyleSearch] = useState('')
+    const [profileSearch, setProfileStyleSearch] = useState('')
 
     const whatToFilterFor = (event) => {
         setFilter(event)
@@ -66,16 +101,22 @@ function FilterDrawer() {
                     <div>
                         <label>Condition: </label>
                         <select name="condition" id="">
-                            <option value="used">Used</option>
-                            <option value="new">New</option>
+                            <option>Select Condition</option>
+                            {condition.map((con) => {
+                                return (
+                                    <option key={con} value={con}>
+                                        {con}
+                                    </option>
+                                );
+                            })}
                         </select>
                     </div>
                     <div>
-                        <label>Brand: </label>
-                        <select name="brand" id="">
-                            <option value="">Select Brand</option>
-                            <option value="burton">Butron</option>
-                            <option value="dalbello">Dalbello</option>
+                        <label>Gender: </label>
+                        <select name="gender" id="">
+                            <option value="">Select Gender</option>
+                            <option value="men">Men</option>
+                            <option value="woman">Women</option>
                         </select>
                     </div>
                     <div>
@@ -83,13 +124,117 @@ function FilterDrawer() {
                         <select name="cat" id="" onChange={(event) => { setCategory(event.target.value) }}>
                             <option value="">Select Category</option>
                             <option value="snowboard">Snowboard</option>
+                            <option value="snowboardBoots">Snowboard Boots</option>
+                            <option value="snowboardBindings">Snowboard Bindings</option>
                             <option value="ski">Ski</option>
+                            <option value="skiBoots">Ski Boots</option>
+                            <option value="skiBindings">Ski Bindings</option>
                             <option value="helmet">Helmet</option>
                             <option value="apparel">Apparel</option>
                         </select>
                     </div>
                     <div>
-                        {category === "snowboard" ? <p>snowboard style</p> : null}
+                        {category === "snowboard" ?
+                            <div>
+                                <label>Board Style: </label>
+                                <select name="snowboardStyle" id="">
+                                    <option>Select Style</option>
+                                    {snowboardStyle.map((style) => {
+                                        return (
+                                            <option key={style} value={style}>
+                                                {style}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            :
+                            null}
+
+                        {category === "ski" ?
+                            <div>
+                                <div>
+                                <label>Ski Style: </label>
+                                <select name="skiStyle" id="">
+                                    <option>Select Style</option>
+                                    {skiStyle.map((style) => {
+                                        return (
+                                            <option key={style} value={style}>
+                                                {style}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                                </div>
+                                <div>
+                                <label>Ski Profile: </label>
+                                <select name="skiProfile" id="">
+                                    <option>Select Profile</option>
+                                    {profile.map((prof) => {
+                                        return (
+                                            <option key={prof} value={prof}>
+                                                {prof}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                                </div>
+                            </div>
+                            :
+                            null}
+
+                        {category === "snowboardBoots" ||
+                            category === "skiBoots" ?
+                            <div>
+                                <label>Lacing System: </label>
+                                <select name="lacing_system" id="">
+                                    <option>Select Laces</option>
+                                    {lacing_system.map((system) => {
+                                        return (
+                                            <option key={system} value={system}>
+                                                {system}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            :
+                            null}
+
+                        {category === "skiBoots" ?
+                            <div>
+                                <label>Boot Flex: </label>
+                                <select name="flex" id="">
+                                    <option>Select Laces</option>
+                                    {flex.map((flex) => {
+                                        return (
+                                            <option key={flex} value={flex}>
+                                                {flex}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            :
+                            null}
+
+                        {category === "helmet" ?
+                            <div>
+                                <label>Helmet Size: </label>
+                                <select name="helmet" id="">
+                                    <option>Select Size</option>
+                                    {size.map((size) => {
+                                        return (
+                                            <option key={size} value={size}>
+                                                {size}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            :
+                            null}
+
                     </div>
 
                 </div>
@@ -100,10 +245,10 @@ function FilterDrawer() {
     );
 
     return (
-        <div>
+        <div className="filterContainer">
             {['top'].map((anchor) => (
                 <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>Filter</Button>
+                    <Button onClick={toggleDrawer(anchor, true)} className="filterBtn">Filter</Button>
                     <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
                         {list(anchor)}
                     </Drawer>
